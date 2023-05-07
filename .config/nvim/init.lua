72fc27fefbce8d67112d862cf2a2b8ccef4c59f8
chuyukang <17082643+Chuyukang@users.noreply.github.com>
2023-05-07 14:15:18 +0800

    nvim: fix nvim-treesitter break change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -276,7 +276,7 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vimdoc', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
